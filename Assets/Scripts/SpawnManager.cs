38e3f6cf4b5acfa4b57bc7c32d13e9d644b37cc0
Thejbm19 <jaumebauza10@gmail.com>
2021-11-29 09:33:51 +0100

    final

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject obstaclePrefab;
+    public Vector3 spawnPos = new Vector3(0, 0, 30);
+    private float randomY;
+    private float startTime = 0.5f;
+    private float repeatRate = 5f;
+    private int Number = 50;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //invoca la funcion cada 5 segundos
+        InvokeRepeating("SpawnObstacle", startTime, repeatRate);
     }
 
-    // Update is called once per frame
-    void Update()
+    // funcion que spawnea los obstaculos
+    public void SpawnObstacle()
     {
-        
+
+        randomY = Random.Range(-10f, 10f); // numero aleatorio entre -10 y 10
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); //funcion para instanciar los obstaculos
+        spawnPos = new Vector3(0, randomY, spawnPos.z + Number); //posicion de spawneo de los obstaculos
+
+
+
+
     }
 }
